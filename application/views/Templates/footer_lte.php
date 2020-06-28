3943ca78aa4c91aa7ce0e00f793853035832575f
xyelea <xyelea@gmail.com>
2020-06-28 19:18:49 +0700

    buat template nav dan footer lte

---
+++ application/views/Templates/footer_lte.php
@@ -0,0 +1,23 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="float-right d-none d-sm-inline">
+		Made With <i class="far fa-heart"></i> By Satria Efriyadi
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2020 <a href=""></a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<script src="<?= base_url('assets'); ?>/style/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('assets'); ?>/style/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets'); ?>/style/dist/js/adminlte.min.js"></script>
+</body>
+
+
+</html>
