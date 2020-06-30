9e0b43393789506b4e0268619a807532617c2e0c
xyelea <xyelea@gmail.com>
2020-06-30 20:57:17 +0700

    update

--- application/views/Templates/nav_informasi.php
+++ application/views/Templates/nav_informasi.php
@@ -111,7 +111,7 @@
 								<li class="nav-item">
 									<a href="<?= base_url('data/informasi_new_normal') ?>" class="nav-link">
 										<i class="fas fa-head-side-virus nav-icon"></i>
-										<p>New Normal</p>
+										<p>AKB</p>
 									</a>
 								</li>
 								<!-- End Dropdown Menu -->
--- application/views/informasi/new_normal.php
+++ application/views/informasi/new_normal.php
@@ -17,12 +17,19 @@
   		<div class="container-fluid">
   			<div class="card card-danger">
   				<div class="card-header">
-  					<h3 class="card-title"><i class="fas fa-head-side-virus"></i> New Normal </h3>
+  					<h3 class="card-title"><i class="fas fa-head-side-virus"></i> Adaptasi Kebiasaan baru (New Normal) </h3>
   				</div>
   				<!-- /.card-header -->
   				<div class="card-body">
   					<!-- we are adding the .class so bootstrap.js collapse plugin detects it -->
-
+  					<strong>Adaptasi kebiasaan Baru</strong> adalah upaya pembiasaan agar masyarakat tetap dapat beraktifitas walaupun sedang dalam keadaan pandemi COVID-19.
+  					<br><br> Adapun kebiasaan Tersebut adalah :<br><br>
+  					<strong> 1. sering cuci tangan pakai sabun<br>
+  						2. pakai masker<br>
+  						3. jaga jarak<br>
+  						4. istirahat cukup<br>
+  						5. rajin olahraga<br>
+  						6. makan makanan bergizi seimbang </strong>
   					<!-- Accordion tutup -->
   				</div>
 
