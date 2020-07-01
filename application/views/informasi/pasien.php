eb946887cc431ff17e336d7e6552e7e7e3f162d1
xyelea <xyelea@gmail.com>
2020-07-01 09:10:21 +0700

    update pasien

--- application/views/informasi/pasien.php
+++ application/views/informasi/pasien.php
@@ -4,7 +4,7 @@
   	<div class="content-header">
   		<div class="container-fluid">
   			<div class="row mb-0">
-  				<div class="col-sm-6">
+  				<div class="col-sm-3">
 
   				</div><!-- /.col -->
   			</div><!-- /.row -->
@@ -18,18 +18,55 @@
   			<div class="card card-danger">
   				<div class="card-header">
   					<h3 class="card-title"><i class="fas fa-shield-virus"></i> Pasien Dan istilah Lainnya </h3>
-  				</div>
+  				</div><br>
   				<!-- /.card-header -->
-  				<div class="card-body">
-  					<!-- we are adding the .class so bootstrap.js collapse plugin detects it -->
+  				<div class="row">
+  					<div class="col-sm-4 ml-auto">
+  						<div class="card card-danger bg-danger">
+  							<div class="card-header text-center ">
+  								<h3 class="card-title">Orang Tanpa Gejala (OTG)</h3>
+  							</div>
+  							<div class="card-body ml-3 text-justify">
+  								<img src="<?= base_url('assets') ?>/img/otg.svg" alt="" srcset="" style="width: 100% ; " height="321">
+  								<p>Orang Tanpa Gejala (OTG) adalah seseorang yang tidak bergejala tapi berisiko tertular virus corona dari pasien Covid-19. Selain itu, OTG memiliki kontak erat dengan kasus positif Covid-19.</p>
+  							</div>
+  							<!-- <div class="card-body text-justify">
 
-  					<!-- Accordion tutup -->
+  							</div> -->
+  						</div>
+  					</div>
+  					<div class="col-sm-4 ml-auto">
+  						<div class="card card-danger bg-danger">
+  							<div class="card-header text-center ">
+  								<h3 class="card-title">Orang dalam Pemantauan (ODP)</h3>
+  							</div>
+  							<div class="card-body m-auto">
+  								<img src="<?= base_url('assets') ?>/img/pasien2.png" alt="" srcset="">
+  							</div>
+  							<div class="card-body text-justify">
+  								Orang Dalam Pemantauan (ODP) COVID-19 adalah orang yang mengalami gejala demam (>38 C) atau ada riwayat demam ATAU ISPA tanpa pneumonia DAN memiliki riwayat perjalanan ke negara atau daerah dengan risiko COVID-19 tinggi pada 14 hari terakhir sebelum timbul gejala
+  							</div>
+  						</div>
+  					</div>
+  					<div class="col-sm-4 ml-auto">
+  						<div class="card card-danger bg-danger">
+  							<div class="card-header text-center ">
+  								<h3 class="card-title">Pasien Dalam Pengawasan (PDP)</h3>
+  							</div>
+  							<div class="card-body ml-5">
+  								<img src="<?= base_url('assets') ?>/img/pasienx.png" alt="" srcset="">
+  							</div>
+  							<div class="card-body text-justify">
+  								Pasien Dalam Pengawasan (PDP) COVID-19 adalah orang mengalami gejala demam (>38 C) atau ada riwayat demam, ISPA, pneumonia ringan hingga berat serta memiliki riwayat perjalanan ke negara yang terjangkit atau kontak dengan orang yang terkonfirmasi positif COVID-19 dalam 14 hari terakhir.
+  							</div>
+  						</div>
+  					</div>
+
+  					<!-- /.card-body -->
   				</div>
-
-  			</div>
-  			<!-- /.card-body -->
+  			</div><!-- /.container-fluid -->
   		</div>
-  	</div><!-- /.container-fluid -->
+  	</div>
   </div>
   <!-- /.content -->
   <!-- /.content-wrapper -->
