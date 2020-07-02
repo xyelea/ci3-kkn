8c286351ba1df49a457d040214b79ac91188fa4d
xyelea <xyelea@gmail.com>
2020-07-02 14:40:00 +0700

    update

--- application/views/informasi/infografis.php
+++ application/views/informasi/infografis.php
@@ -25,249 +25,187 @@
 
   					<!-- we are adding the .class so bootstrap.js collapse plugin detects it -->
   					<div class="row">
+  						<!-- Gugus Tugas Covid-19 -->
   						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://covid19.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Gugus tugas COVID-19 ( Nasional)
-  										</a>
-  									</h4>
-  								</div>
+  							<div class="card">
+  								<a href="https://covid19.go.id/" target="_blank">
+  									<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Gugus Tugas Covid-19</button>
+  								</a>
   							</div>
   						</div>
+  						<!-- DKI Jakarta -->
+  						<div class="col-sm-4 ml-auto">
+  							<div class="card">
+  								<a href="https://corona.jakarta.go.id/" target="_blank">
+  									<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> DKI Jakarta</button>
+  								</a>
+  							</div>
+  						</div>
+  						<!-- Bengkulu -->
+  						<div class="col-sm-4 ml-auto">
+  							<div class="card">
+  								<a href="http://covid19.bengkulukota.go.id/" target="_blank">
+  									<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Bengkulu</button>
+  								</a>
+  							</div>
+  						</div>
+  						<!-- Jabar -->
+  						<div class="row">
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://pikobar.jabarprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Jawa Barat</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Jogja -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://corona.jogjaprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> DI Yogyakarta</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Jateng -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://corona.jatengprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Jawa Tengah</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Sumbar -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://corona.sumbarprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Sumatera Barat</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Bali -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://infocorona.baliprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Bali</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Jambi -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://corona.jambiprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Jambi</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Babel -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://covid19.babelprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Bangka Belitung</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Maluku -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://corona.malukuprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Maluku</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Sumsel -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://corona.sumselprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Sumatera Selatan</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- sulteng -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://dinkes.sultengprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Sulawesi Tengah</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- jatim -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://infocovid19.jatimprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Jawa Timur</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- aceh -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://covid19.acehprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Aceh</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- sulbar -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://covid19.sulbarprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Sulawesi Barat</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- sumut -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://covid19.sumutprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Sumatera Utara</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- kepri -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://corona.kepriprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Kepulauan Riau</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- kalsel -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="http://corona.kalselprov.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i> Kalimantan Selatan</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- Papua -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i>Papua
+  											&nbsp;Call Center : 1500671</button>
+  									</a>
+  								</div>
+  							</div>
+  							<!-- kalteng -->
+  							<div class="col-sm-4 ml-auto">
+  								<div class="card">
+  									<a href="https://corona.kalteng.go.id/" target="_blank">
+  										<button type="button" class="btn btn-block btn-outline-danger btn-lg elevation-3"><i class="fas fa-globe-asia"></i>Kalimantan Tengah</button>
+  									</a>
+  								</div>
+  							</div>
 
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.jakarta.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> DKI Jakarta
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
   						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://covid19.bengkulukota.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Bengkulu
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://pikobar.jabarprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Jawa Barat
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.jogjaprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> DI Yogyakarta
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.jatengprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Jawa Tengah
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.sumbarprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Sumatera Barat
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://infocorona.baliprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Bali
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://corona.jambiprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Jambi
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://covid19.babelprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Bangka Belitung
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.malukuprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Maluku
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://corona.sumselprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Sumatera Selatan
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://dinkes.sultengprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Sulawesi Tengah
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://infocovid19.jatimprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Jawa Timur
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://covid19.acehprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Aceh
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://covid19.sulbarprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Sulawesi Barat
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://covid19.sumutprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Sumatera Utara
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://corona.kepriprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Kepulauan Riau
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.jakarta.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Jakarta
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="http://corona.kalselprov.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Kalimantan Selatan
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="" target="_blank"><i class="fas fa-globe-asia"></i> Papua <br>
-  											&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;Call Center : 1500671
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-8 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.kalteng.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Kalimantan Tengah
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
-  						<div class="col-sm-4 ml-auto">
-  							<div class="card card-danger elevation-3">
-  								<div class="card-header">
-  									<h4 class="card-title">
-  										<a href="https://corona.kalteng.go.id/" target="_blank"><i class="fas fa-globe-asia"></i> Kepulauan Riau
-  										</a>
-  									</h4>
-  								</div>
-  							</div>
-  						</div>
+  						<!-- Accordion tutup -->
 
+  						<!-- Accordion tutup -->
   					</div>
-  					<!-- Accordion tutup -->
 
-  					<!-- Accordion tutup -->
   				</div>
-
+  				<!-- /.card-body -->
   			</div>
-  			<!-- /.card-body -->
-  		</div>
-  	</div><!-- /.container-fluid -->
-  </div>
-  <!-- /.content -->
-  <!-- /.content-wrapper -->
+  		</div><!-- /.container-fluid -->
+  	</div>
+  	<!-- /.content -->
+  	<!-- /.content-wrapper -->
